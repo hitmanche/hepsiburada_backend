12f88df9e9c258b638468872ee4607377d4f7639
tayfun akgün <tayfun_akgun@windowslive.com>
2021-11-14 17:27:28 +0300

    Redis url updated

--- routes/products.js
+++ routes/products.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 var data = require("../data/index");
 //production redis url
-let redis_url = process.env.REDIS_URL;
+let redis_url = "redis://:p93166e436ee9118df2f692cc320a187e79c6507224de17c17ac5f6e68a39afbd@ec2-52-49-169-28.eu-west-1.compute.amazonaws.com:19129";
 if (process.env.ENVIRONMENT === "development") {
   redis_url = "redis://localhost";
 }
