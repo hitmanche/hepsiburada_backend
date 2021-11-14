39bfb0790e874310376d8ce1b68398bfc2bae082
tayfun akgün <tayfun_akgun@windowslive.com>
2021-11-15 02:35:09 +0300

    Updated.

--- routes/products.js
+++ routes/products.js
@@ -1,15 +1,12 @@
 var express = require("express");
 var router = express.Router();
 var dataJson = require("../data/index");
-//production redis url
+
 let redis_url =
   "redis://:p93166e436ee9118df2f692cc320a187e79c6507224de17c17ac5f6e68a39afbd@ec2-52-49-169-28.eu-west-1.compute.amazonaws.com:19129";
-if (process.env.ENVIRONMENT === "development") {
-  redis_url = "redis://localhost";
-}
-//redis setup
+
 let client = require("redis").createClient(redis_url, {
-  connect_timeout: 10000,
+  connect_timeout: 100,
 });
 
 router.get("/", function (req, res, next) {
@@ -96,8 +93,6 @@ router.get("/", function (req, res, next) {
             break;
         }
       }
-
-      data = data.slice((req.query.pageNumber - 1) * req.query.pageNumber, 12 * req.query.pageNumber);
     }
 
     res.send({ data, filters });
